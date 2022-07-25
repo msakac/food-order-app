aba3ab667ff2dbee540939f5cffbcf44f32f1b22
Martin Sakac <msakac@foi.hr>
2022-07-25 15:01:41 +0200

    Added animation with useEffect to header cart button

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,17 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 function HeaderCartButton({ onSetShown }) {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const ctx = useContext(CartContext);
 
     const numOfCartItems = ctx.items.reduce((currentValue, item) => { 
         return currentValue + item.amount; 
     }, 0);
 
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
+
+    const {items} = ctx;
+
+    useEffect(()=>{
+        if(items.length === 0 ){
+            return;
+        }
+        setBtnIsHighlighted(true);
+        
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items])
+
     return (
-        <button className={classes.button} onClick={onSetShown}>
+        <button className={btnClasses} onClick={onSetShown}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
