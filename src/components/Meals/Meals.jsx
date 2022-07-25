7089d28154a8cf060d308b5a7c0fe657eb61cf03
Martin Sakac <msakac@foi.hr>
2022-07-25 12:11:58 +0200

    Added AvailableMeals, MealsSummary and Meals component

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,11 @@
+import AvailableMeals from "./AvailableMeals/AvailableMeals";
+import MealsSummary from "./MealsSummary/MealsSummary";
+
+function Meals() {
+    return <>
+        <MealsSummary />
+        <AvailableMeals />
+    </>
+}
+
+export default Meals;
