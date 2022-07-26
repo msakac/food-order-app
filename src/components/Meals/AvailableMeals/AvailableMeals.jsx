5d3faad95670683cc52047ee2ebe644a7142eb68
Martin Sakac <msakac@foi.hr>
2022-07-26 13:18:39 +0200

    Added loading indicator while fetching the data

--- src/components/Meals/AvailableMeals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals/AvailableMeals.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 
 function AvailableMeals() {
 
-    const [isLoaded, setIsLoaded] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     const [meals, setMeals] = useState([]);
 
     useEffect(() => {
@@ -22,9 +22,16 @@ function AvailableMeals() {
                 })
             }
             setMeals(loadedMeals);
+            setIsLoading(false);
         }
         fetchMeals()
     }, [])
+
+    if(isLoading){
+        return <section className={classes.MealsLoading}>
+            <p>Loading...</p>
+        </section>
+    }
 
     const mealsList = meals.map(meal => <MealItem key={meal.id} meal={meal} />);
 
