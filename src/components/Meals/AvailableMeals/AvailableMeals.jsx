c313286a30cc4cfad231780f74765a9c0f8b0a60
Martin Sakac <msakac@foi.hr>
2022-07-26 13:12:54 +0200

    Fetching data from firebase database

--- src/components/Meals/AvailableMeals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals/AvailableMeals.jsx
@@ -1,45 +1,41 @@
 import Card from '../../UI/Card/Card';
 import MealItem from '../MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
-
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+import { useEffect, useState } from 'react';
 
 function AvailableMeals() {
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} meal={meal}/>);
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('https://react-daf75-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+            const responseData = await response.json();
+            const loadedMeals = [];
+            for(const key in responseData){
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price,
+                })
+            }
+            setMeals(loadedMeals);
+        }
+        fetchMeals()
+    }, [])
+
+    const mealsList = meals.map(meal => <MealItem key={meal.id} meal={meal} />);
 
     return <section className={classes.meals}>
-            <Card>
+        <Card>
             <ul>
                 {mealsList}
             </ul>
-            </Card>
-        </section>
-    
+        </Card>
+    </section>
+
 }
 
 export default AvailableMeals;
